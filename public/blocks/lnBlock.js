cc126f4970f5469fa6f43b5405019c9221facc8b
simosathan9 <simosathan03@gmail.com>
2024-09-14 13:17:59 +0300

    New blocks and enhancenments on existing ones

--- public/blocks/lnBlock.js
+++ public/blocks/lnBlock.js
@@ -1,30 +1,69 @@
 var lnBlock = {
   type: 'ln',
   category: 'File and Directory Operations',
-  message0: 'ln %1 %2',
+  message0: '%{BKY_LN_MESSAGE}',
   unix_description: [
     {
-      command: 'ln %TARGET %LINK'
+      command: 'ln',
+      symbolic: '-s',
+      force: '-f',
+      interactive: '-i',
+      verbose: '-v',
+      follow_symlink: '-L',
+      no_symlink_follow: '-P'
     }
   ],
-  args0: [
+  message1: '%{BKY_LN_SYMBOLIC_LINK}',
+  args1: [
+    {
+      type: 'field_checkbox',
+      name: 'symbolic',
+      checked: false // by default it's disabled
+    }
+  ],
+  message2: '%{BKY_LN_FORCE}',
+  args2: [
+    {
+      type: 'field_checkbox',
+      name: 'force',
+      checked: false // by default it's disabled
+    }
+  ],
+  message3: '%{BKY_LN_INTERACTIVE}',
+  args3: [
+    {
+      type: 'field_checkbox',
+      name: 'interactive',
+      checked: false // by default it's disabled
+    }
+  ],
+  message4: '%{BKY_LN_VERBOSE}',
+  args4: [
+    {
+      type: 'field_checkbox',
+      name: 'verbose',
+      checked: false // by default it's disabled
+    }
+  ],
+
+  message5: '%{BKY_LN_SOURCE}: %1 %{BKY_LN_TARGET}: %2',
+  args5: [
+    {
+      type: 'field_input',
+      name: 'SOURCE',
+      text: 'source' // default source
+    },
     {
       type: 'field_input',
       name: 'TARGET',
       text: 'target' // default target
-    },
-    {
-      type: 'field_input',
-      name: 'LINK',
-      text: 'link' // default link
     }
   ],
   style: 'File and Directory Operations',
   previousStatement: 'Action',
   nextStatement: 'Action',
-  tooltip: 'Δημιουργεί συνδέσμους (links) μεταξύ αρχείων.',
+  tooltip: '%{BKY_LN_TOOLTIP}',
   helpUrl: 'https://linux.die.net/man/1/ln'
 };
 
 Blockly.defineBlocksWithJsonArray([lnBlock]);
-//
