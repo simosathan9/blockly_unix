268390bd5cc15f1540bda4b8c2204f73bef5d22f
simosathan9 <simosathan03@gmail.com>
2024-10-04 19:29:32 +0300

    Fixes in filesystem operations blocks

--- public/blocks/rmBlock.js
+++ public/blocks/rmBlock.js
@@ -53,7 +53,23 @@ var rmBlock = {
   extensions: [],
   style: 'Filesystem Operations',
   tooltip: '%{BKY_RM_TOOLTIP}',
-  helpUrl: 'https://linux.die.net/man/1/rm'
+  helpUrl: 'https://linux.die.net/man/1/rm',
+  generateCommand: function (block) {
+    let rmCommand = 'rm ';
+    const force = block.getFieldValue('force') === 'TRUE';
+    const requestConfirmation =
+      block.getFieldValue('request_confirmation') === 'TRUE';
+    const removeDirectory = block.getFieldValue('remove_directory') === 'TRUE';
+    const recursive = block.getFieldValue('recursive') === 'TRUE';
+    if (force) rmCommand += '-f ';
+    if (requestConfirmation) rmCommand += '-i ';
+    if (removeDirectory) rmCommand += '-d ';
+    if (recursive) rmCommand += '-R ';
+    const argumentBlock = block.getInputTargetBlock('ARGUMENT');
+    rmCommand += ' ' + handleArgumentsBlocks(argumentBlock);
+    generatedCommand = rmCommand;
+    return generatedCommand;
+  }
 };
 
 Blockly.defineBlocksWithJsonArray([rmBlock]);
